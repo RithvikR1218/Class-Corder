493dcf53d10ffb0c9865f640dc175ebb6826450f
Rthvik <rithvik.ravilla@gmail.com>
2025-04-22 02:20:30 -0500

    feat: h265

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ app = create_app()
 
 if __name__ == '__main__':
     import logging
-    logging.basicConfig(level=logging.INFO)
+    # logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)  # Set log level to DEBUG
     
     app.run(host='0.0.0.0', port=9000, debug=True)
